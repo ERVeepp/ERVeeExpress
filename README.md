5095e509c2384a5a5bfe3e52dec03de044659481
王伟 <736354882@qq.com>
2017-02-09 11:49:17 +0800

    配置环境
    20170209

---
+++ README.md
@@ -0,0 +1,19 @@
+# vue
+
+> A Vue.js project
+
+## Build Setup
+
+``` bash
+# 由于本项目需要运行在服务器环境下，所以需要在本地搭建服务器环境，在此选用个人比较熟悉的node服务器，首先clone项目到本地
+# 进入到myapp文件夹下，按住shift键并点击鼠标右键，然后点击在此处打开命令窗口，输入以下命令，安装项目依赖
+npm install
+# 运行express服务器
+npm start
+# 浏览器地址栏输入以下地址，即为项目页面
+http://localhost:3000/
+```
+
+## License
+
+[MIT](http://opensource.org/licenses/MIT)
--- myapp/app - 副本.js
+++ myapp/app - 副本.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
-//var my = require('./routes/my');
+var my = require('./routes/my');
 
 var app = express();
 
@@ -33,7 +33,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
-//app.use('/my', my);
+app.use('/my', my);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
